f17a2b0b450e3edf88c37e5bccb7bb31c7d67ae5
Alexander Elbs <alex@segv.de>
2023-12-07 22:16:07 +0100

    day 4, part 2

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -6,6 +6,7 @@ struct Card {
     cardnum: u8,
     win: Vec<u8>,
     got: Vec<u8>,
+    count: u32,
 }
 
 impl Card {
@@ -25,21 +26,25 @@ impl Card {
                 .filter(|x| !x.is_empty())
                 .map(|x| x.parse::<u8>().unwrap())
                 .collect::<Vec<u8>>(),
+            count: 1,
         }
     }
 
-    fn points(&self) -> u32 {
-        let count: u32 = self
-            .got
+    fn wins(&self) -> u32 {
+        self.got
             .iter()
             .filter(|x| self.win.contains(x))
             .count()
             .try_into()
-            .unwrap();
+            .unwrap()
+    }
 
-        match count {
+    fn points(&self) -> u32 {
+        let wins: u32 = self.wins();
+
+        match wins {
             0 => 0,
-            _ => 2_u32.pow(count - 1),
+            _ => 2_u32.pow(wins - 1),
         }
     }
 }
@@ -58,16 +63,39 @@ impl Cards {
         }
     }
 
+    fn len(&self) -> usize {
+        self.cards.len()
+    }
+
     fn points(&self) -> u32 {
         self.cards.iter().map(|x| x.points()).sum()
+    }
+
+    fn calc_copies(mut self) -> Self {
+        for i in 0..self.len() {
+            for index in self.cards[i].cardnum as usize
+                ..self.cards[i].cardnum as usize + self.cards[i].wins() as usize
+            {
+                if index >= self.len() {
+                    break;
+                }
+                self.cards[index].count += self.cards[i].count;
+            }
+        }
+        self
+    }
+
+    fn count(&self) -> u32 {
+        self.cards.iter().map(|card| card.count).sum()
     }
 }
 
 fn main() {
     let input = fs::read_to_string("4.input").expect("Should have been able to read the file");
-    let cards = Cards::new(&input);
+    let cards = Cards::new(&input).calc_copies();
 
     println!("total points: {}", cards.points());
+    println!("total cards: {}", cards.count());
 }
 
 #[cfg(test)]
@@ -82,6 +110,7 @@ mod tests {
                 cardnum: 1,
                 win: vec![41, 48, 83, 86, 17],
                 got: vec![83, 86, 6, 31, 17, 9, 48, 53],
+                count: 1
             }
         );
     }
@@ -96,6 +125,7 @@ mod tests {
                 cardnum: 1,
                 win: vec![41, 48, 83, 86, 17],
                 got: vec![83, 86, 6, 31, 17, 9, 48, 53],
+                count: 1
             }
         );
 
@@ -116,5 +146,29 @@ mod tests {
         let cards = Cards::new(&input);
 
         assert_eq!(cards.points(), 13);
+    }
+
+    #[test]
+    fn test_calc_copies() {
+        let input =
+            fs::read_to_string("4sample.input").expect("Should have been able to read the file");
+        let cards = Cards::new(&input).calc_copies();
+
+        assert_eq!(cards.cards[0].count, 1);
+        assert_eq!(cards.cards[1].count, 2);
+        assert_eq!(cards.cards[2].count, 4);
+        assert_eq!(cards.cards[3].count, 8);
+        assert_eq!(cards.cards[4].count, 14);
+        assert_eq!(cards.cards[5].count, 1);
+    }
+
+    #[test]
+    fn test_cards_count() {
+        let input =
+            fs::read_to_string("4sample.input").expect("Should have been able to read the file");
+        let cards = Cards::new(&input);
+        let cards = cards.calc_copies();
+
+        assert_eq!(cards.count(), 30);
     }
 }
